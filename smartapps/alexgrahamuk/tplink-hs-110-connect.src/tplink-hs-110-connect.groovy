b878631f6a3b7b9d37606270744a1e760e32b074
alexgrahamuk <alex@a-graham.com>
2017-01-07 16:10:49 +0000

    Stubbed out app a bit more

--- smartapps/alexgrahamuk/tplink-hs-110-connect.src/tplink-hs-110-connect.groovy
+++ smartapps/alexgrahamuk/tplink-hs-110-connect.src/tplink-hs-110-connect.groovy
@@ -3,7 +3,7 @@ definition(
         namespace: "alexgrahamuk",
         author: "Alex Graham",
         description: "Allows you to connect your HS-110 energy monitored sockets using the alexgrahamuk/hs-110-server.",
-        category: "Bridges", //check me
+        category: "My Apps",
         iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/hue.png", //change me
         iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/hue@2x.png", //change me
         singleInstance: true
@@ -11,7 +11,26 @@ definition(
 
 
 preferences {
-    input(name: "devices", type: "device.tplink-hs-110", title: "HS-110 Switches", required: true, displayDuringSetup: true, multiple: true)
-    input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
-    input("gatewayPort", "text", title: "Gateway Port", required: true, displayDuringSetup: true)
+    section("Devices") {
+        input(name: "devices", type: "device.tplink-hs-110", title: "HS-110 Switches", required: true, displayDuringSetup: true, multiple: true)
+    }
+    section("Gateway") {
+        input("gatewayIP", "text", title: "Gateway IP", required: true, displayDuringSetup: true)
+        input("gatewayPort", "text", title: "Gateway Port", required: true, displayDuringSetup: true)
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+ //   unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // TODO: subscribe to attributes, devices, locations, etc.
 }
